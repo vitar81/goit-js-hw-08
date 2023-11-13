504491e119c63d84ee09ed44485d352938447327
vitar81 <vitar81@gmail.com>
2023-11-13 19:50:04 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items";
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
+
+function getGallery() {
+    const galleryItemsMarkup = galleryItems
+    .map(
+    ({ preview, original, description }) =>
+        `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img 
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}" />
+    </a>
+</li>`
+    )
+    .join("");
+
+galleryList.innerHTML = galleryItemsMarkup;
+}
+
+getGallery();
+
+const lightbox = new SimpleLightbox(".gallery__link", {
+    captionsData: "alt",
+    captionsDelay: 250,
+    captions: true,
+});
 
 console.log(galleryItems);
