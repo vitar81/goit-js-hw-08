26bd0c779408a576bdc6b713229384441a4155bb
vitar81 <vitar81@gmail.com>
2023-11-14 18:10:22 +0200

    02 add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import VimeoPlayer from '@vimeo/player/dist/player.es.js';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+
+const keyStorage = 'videoplayer-current-time';
+const player = new VimeoPlayer(iframe);
+
+const saveTimeToLocalStorage = throttle(time => {
+    localStorage.setItem(keyStorage, time);
+}, 1000);
+
+player.on('timeupdate', function (data) {
+    const currentTime = data.seconds;
+    saveTimeToLocalStorage(currentTime);
+});
+
+window.addEventListener('load', () => {
+    const saveTime = localStorage.getItem(keyStorage);
+
+    if (saveTime) {
+    player.setCurrentTime(parseFloat(saveTime));
+    }
+});
