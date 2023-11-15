457b570943d4731fb56e6538a63ddb489ed1dd1f
vitar81 <vitar81@gmail.com>
2023-11-15 18:53:27 +0200

    remote

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,15 +6,15 @@ form.addEventListener('submit', onSubmitForm);
 
 const formData = {};
 
-function onFormData() {
-  formData(target.name) = target.value;
+function onFormData(e) {
+  formData[e.target.name] = e.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
-function onSubmitForm() {
+function onSubmitForm(e) {
   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-  preventDefault();
-  currentTarget.reset();
+  e.preventDefault();
+  e.currentTarget.reset();
   localStorage.removeItem('feedback-form-state');
 }
 
