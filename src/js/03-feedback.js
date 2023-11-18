8428aacd491af30d65b9876b7f265cb8bfeecb5c
vitar81 <vitar81@gmail.com>
2023-11-18 13:37:38 +0200

    add 03 correct submit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,49 +1,5 @@
 import throttle from 'lodash.throttle';
 
-const formEl = document.querySelector('.feedback-form');
-const keyStorage = 'feedback-form-state';
-
-let formData = {};
-
-const saveToLocalStorage = throttle(e => {
-  formData[e.target.name] = e.target.value.trim();
-  localStorage.setItem(keyStorage, JSON.stringify(formData));
-}, 500);
-
-formEl.addEventListener('input', saveToLocalStorage);
-formEl.addEventListener('submit', handleSubmit);
-document.addEventListener('DOMContentLoaded', loadSavedData);
-
-function loadSavedData() {
-  try {
-    const data = localStorage.getItem(keyStorage);
-    if (!data) return;
-    formData = JSON.parse(data);
-    Object.entries(formData).forEach(([key, val]) => {
-      formEl.elements[key].value = val;
-    });
-  } catch (error) {
-    console.log(error.message);
-  }
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  const formFields = formEl.elements;
-  const isFormValid = Array.from(formFields).every(
-    field => field.value.trim() !== ''
-  );
-
-  if (!isFormValid) {
-    alert('Please fill in all fields before submitting.');
-    return;
-  }
-
-  localStorage.removeItem(keyStorage);
-  console.log(formData);
-  formData = {};
-  event.target.reset();
-}
 document.addEventListener('DOMContentLoaded', function () {
   const form = document.querySelector('.feedback-form');
   const emailInput = form.querySelector('input[name="email"]');
